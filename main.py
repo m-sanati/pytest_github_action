db8a4da233d535ba31919983058a38b3e8c9ca5a
mahsa.sanati <mahsa.sanati@zattoo.com>
2024-02-27 16:25:54 +0100

    test.yaml added

--- main.py
+++ main.py
@@ -1,13 +1,4 @@
-import pytest
-
-from main import Person
-
-
-@pytest.fixture
-def person():
-    return Person(name="Mahsa", age=27)
-
-
-def test_person(person):
-    assert person.name == "Mahsa"
-    assert person.age == 27
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
